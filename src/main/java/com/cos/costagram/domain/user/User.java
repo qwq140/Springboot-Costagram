0919ad2f3bd9837806b6c23add285e34602225ac
unknown <qwq140@gmail.com>
2021-03-22 12:48:21 +0900

    3. instagram clone - User Entity 생성 완료

---
+++ src/main/java/com/cos/costagram/domain/user/User.java
@@ -0,0 +1,45 @@
+package com.cos.costagram.domain.user;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(length = 30, unique = true)
+	private String username;
+	
+	@JsonIgnore
+	private String password;
+	
+	private String name; // 이름
+	private String webSite; // 자기 홈페이지
+	private String bio; // 자기소개
+	private String email;
+	private String phone;
+	private String gender;
+	
+	private String profileImage;
+	private String provider; // 제공자 Google, Facebook, Naver OAuth login
+	
+	private String role; //USER, ADMIN
+	
+	@CreationTimestamp
+	private Timestamp createDate;
+}
