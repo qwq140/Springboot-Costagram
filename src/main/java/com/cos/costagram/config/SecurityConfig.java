9a58c81f88a0f140260c4b7c0c13128c549841f2
unknown <qwq140@gmail.com>
2021-03-22 12:39:29 +0900

    2. instagram clone - security setting complet (OAuth2.0, CORS 추후 진행)

---
+++ src/main/java/com/cos/costagram/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.cos.costagram.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Bean
+	public BCryptPasswordEncoder encode() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	// model : Image, User, Likes, Follow, Tag : need authentication 
+	// auth address : don't need authentication
+	// static
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests()
+			.antMatchers("/", "/user/**", "/image/**", "/follow/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+			.anyRequest()
+			.permitAll()
+			.and()
+			.formLogin()
+			.loginPage("/auth/loginForm")
+			.loginProcessingUrl("/");
+			// OAuth2.0과 CORS는 나중에
+	}
+	
+}
