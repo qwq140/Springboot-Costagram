fc20a68fffc14e945bce36d52ee63f764b9a0766
unknown <qwq140@gmail.com>
2021-03-23 10:33:36 +0900

    5. sign up complet

--- src/main/java/com/cos/costagram/domain/user/User.java
+++ src/main/java/com/cos/costagram/domain/user/User.java
@@ -12,9 +12,15 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 public class User {
 
--- src/main/java/com/cos/costagram/web/AuthController.java
+++ src/main/java/com/cos/costagram/web/AuthController.java
@@ -2,10 +2,23 @@ package com.cos.costagram.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cos.costagram.domain.user.User;
+import com.cos.costagram.service.AuthService;
+import com.cos.costagram.utils.Script;
+import com.cos.costagram.web.auth.dto.UserJoinReqDto;
+
+import lombok.RequiredArgsConstructor;
 
 // start address : /auth
+@RequiredArgsConstructor
 @Controller
 public class AuthController {
+	
+	private final AuthService authService;
+	
 	@GetMapping("/auth/loginForm")
 	public String loginForm() {
 		return "auth/loginForm";
@@ -13,5 +26,11 @@ public class AuthController {
 	@GetMapping("/auth/joinForm")
 	public String joinForm() {
 		return "auth/joinForm";
+	}
+	@PostMapping("/auth/join")
+	public @ResponseBody String join(UserJoinReqDto userJoinReqDto) {
+		User userEntity = userJoinReqDto.toEntity();
+		authService.회원가입(userEntity);
+		return Script.href("성공", "/auth/loginForm");
 	}
 }
